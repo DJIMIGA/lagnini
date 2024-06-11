c663114d905b69e00ae9ef6b15728b563b1d1b44
aly <pymalien@gmail.com>
2024-06-11 11:06:17 +0200

    make it better resolve merge conflicts

--- Univers/settings.py
+++ Univers/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_data
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,12 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -142,5 +146,5 @@ DATE_FORMAT = 'd/m/Y'
 DATETIME_FORMAT = 'd/m/Y'
 
 
-
+django_heroku.settings(locals())
 
